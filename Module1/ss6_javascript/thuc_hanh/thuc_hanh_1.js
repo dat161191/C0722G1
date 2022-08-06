26014086a60807a3b49c4e860c34af05e962787a
unknown <you@example.com>
2022-08-06 13:54:32 +0700

    HTML

---
+++ Module1/ss6_javascript/thuc_hanh/thuc_hanh_1.js
@@ -0,0 +1,3 @@
+function sieunhan()
+{ document.getElementsByClass("demo1").innerHTML='<img src="https://1.bp.blogspot.com/-qb5RnoDM7M0/XoBSqkgLSyI/AAAAAAAAadE/xXw77lwnLSsJCO-Q6_jO1AFWZHy9H_oDwCLcBGAsYHQ/s1600/Hinh-anh-con-gai-cute-de-thuong%2B%252874%2529.jpg">';
+}
