b39560e5fbe9dc4cd936d5a89dd45f4d98e46dee
unknown <you@example.com>
2022-09-14 17:19:18 +0700

    Java

---
+++ Module2/src/ss7_abstrac_class_interface/exercise/colorable/Geomatix.java
@@ -0,0 +1,6 @@
+package ss7_abstrac_class_interface.exercise.colorable;
+
+public abstract class Geomatix {
+    public abstract double getArena();
+}
+
---
+++ Module2/src/ss7_abstrac_class_interface/exercise/interface_resizeable/Geomatix1.java
@@ -0,0 +1,6 @@
+package ss7_abstrac_class_interface.exercise.interface_resizeable;
+
+public abstract class Geomatix1 {
+    public abstract double getArena();
+    public abstract String name();
+}
