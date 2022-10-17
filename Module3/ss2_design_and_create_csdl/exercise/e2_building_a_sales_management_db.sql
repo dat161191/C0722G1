ee960fa97a1dac7eed15af9eb35f29d10048b5a0
unknown <you@example.com>
2022-10-17 19:18:17 +0700

    SQL

--- Module3/ss2_design_and_create_csdl/exercise/e2_building_a_sales_management_db.sql
+++ Module3/ss2_design_and_create_csdl/exercise/e2_building_a_sales_management_db.sql
@@ -1,20 +1,24 @@
 DROP DATABASE IF EXISTS sales_manager;
 CREATE DATABASE IF NOT EXISTS sales_manager;
 USE sales_manager;
+
 CREATE TABLE customer (
 c_id INT PRIMARY KEY,
 c_name VARCHAR(50) NOT NULL,
 c_age INT NOT NULL);
+
 CREATE TABLE product (
 p_id INT PRIMARY KEY,
 p_name VARCHAR(50) NOT NULL,
 p_price DOUBLE NOT NULL);
+
 CREATE TABLE oder (
 o_id INT PRIMARY KEY,
 c_id INT NOT NULL,
 o_date DATE NOT NULL,
 o_total_price DOUBLE NOT NULL,
 CONSTRAINT fk_customer FOREIGN KEY (c_id) REFERENCES customer(c_id));
+
 CREATE TABLE oder_dentail (
 o_id INT NOT NULL,
 p_id INT NOT NULL,
