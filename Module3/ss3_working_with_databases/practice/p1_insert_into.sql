9f6f557516d5c9006471360131fd1ff0ca37a5d0
unknown <you@example.com>
2022-10-18 10:42:18 +0700

    SQL

--- Module3/ss3_working_with_databases/practice/p1_insert_into.sql
+++ Module3/ss3_working_with_databases/practice/p1_insert_into.sql
@@ -1,25 +1,25 @@
 USE ss2_quan_ly_sinh_vien;
-INSERT INTO Class
+INSERT INTO class
 VALUES (1, 'A1', '2008-12-20', 1);
 INSERT INTO Class
 VALUES (2, 'A2', '2008-12-22', 1);
 INSERT INTO Class
 VALUES (3, 'B3', current_date, 0);
 
-INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
+INSERT INTO student (student_name, address, phone, status, class_id)
 VALUES ('Hung', 'Ha Noi', '0912113113', 1, 1);
-INSERT INTO Student (StudentName, Address, Status, ClassId)
+INSERT INTO student (student_name, address, status, class_id)
 VALUES ('Hoa', 'Hai phong', 1, 1);
-INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
+INSERT INTO student (student_name, address, phone, status, class_id)
 VALUES ('Manh', 'HCM', '0123123123', 0, 2);
 
-INSERT INTO Subject
+INSERT INTO subject
 VALUES (1, 'CF', 5, 1),
        (2, 'C', 6, 1),
        (3, 'HDJ', 5, 1),
        (4, 'RDBMS', 10, 1);
 
-INSERT INTO Mark (SubId, StudentId, Mark, ExamTimes)
-VALUES (1, 1, 8, 1),
+INSERT INTO mark (sub_id, student_id, mark, exam_times)
+VALUES (1, 1, 10, 1),
        (1, 2, 10, 2),
-       (2, 1, 12, 1);
+       (2, 1, 10, 1);
